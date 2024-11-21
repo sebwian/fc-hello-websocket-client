72ed8ecf464f2dd33823d279815308edf35fd398
Kim seong woo <thewall.ksw@gmail.com>
2024-11-21 13:39:48 +0900

    initialized project

---
+++ src/main/kotlin/pe/swkim/fchellowebsocketclient/FcHelloWebsocketClientApplication.kt
@@ -0,0 +1,11 @@
+package pe.swkim.fchellowebsocketclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcHelloWebsocketClientApplication
+
+fun main(args: Array<String>) {
+    runApplication<FcHelloWebsocketClientApplication>(*args)
+}
---
+++ src/test/kotlin/pe/swkim/fchellowebsocketclient/FcHelloWebsocketClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package pe.swkim.fchellowebsocketclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcHelloWebsocketClientApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
